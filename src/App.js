0caf864348ba5b70d7c7b6b565846cb9836884ae
ldupas <louisdupas@net-c.com>
2020-09-29 18:20:10 +0200

    files and routing created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>
     </div>
   );
 }
